c1233f76fdc31d1001ec5bb4a67391d82c54fb79
Thiago <thiagoleonardo1995@live.com>
2023-11-30 12:27:43 -0300

    Projeto criado

---
+++ src/entities/Account.java
@@ -0,0 +1,47 @@
+
+package entities;
+
+public class Account {
+	// Juros de depósito
+	public static double DEPOSIT_FEE_PERCENTAGE = 0.02;
+	
+	private Long id;
+	private Double balance;
+	
+	public Account() {}
+	
+	public Account(Long id, Double balance) {
+		super();
+		this.id = id;
+		this.balance = balance;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(Double amount) {
+		if(amount > 0) {
+			amount -= amount*DEPOSIT_FEE_PERCENTAGE;
+			balance += amount;	
+		}
+	}
+	
+	public void withdraw(Double amount) {
+		if(amount > balance) {
+			throw new IllegalArgumentException();
+		}
+		amount -= amount*DEPOSIT_FEE_PERCENTAGE;
+		balance += amount;	
+	}
+	
+	public double fullWithdraw() {
+		Double aux = balance;
+		balance = 0.0;
+		return aux;
+	}
+}
