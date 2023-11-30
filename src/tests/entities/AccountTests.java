c170b03fdf09d13e1981be02f1b9cd20382b4e5e
Thiago <thiagoleonardo1995@live.com>
2023-11-30 17:53:16 -0300

    Teste de Depósito com valor negativo

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.Test;
 import entities.Account;
 
 public class AccountTests {
+	/*
+	 * Implementação com padrão AAA:
+	 * Arrange: instancie os objetos necessários
+	 * Act: execute as ações necessárias
+	 * Assert: declare o que deveria acontecer (resultado esperado)
+	 */
 	
 	// Teste de depósito e desconto de taxa
-	
 	@Test		// Nome padrão <AÇAO> should <EFEITO> [when <CENARIO>]
 	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
-		/*
-		 * Implementação com padrão AAA:
-		 * Arrange: instancie os objetos necessários
-		 * Act: execute as ações necessárias
-		 * Assert: declare o que deveria acontecer (resultado esperado)
-		 */
 		
 		// Arrange
 		double amount = 200.0;
@@ -26,8 +25,23 @@ public class AccountTests {
 		// Act
 		acc.deposit(amount);		
 		
-		// Assert: declare o que deveria acontecer (resultado esperado)
+		// Assert
 		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	
+	// deposito não faz nada quando quantia for negativa
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		// Arrange
+		double expectedValue = 100.0;
+		Account acc = new Account(1L, expectedValue);
+		double amount = -200.0;
+		
+		// Act
+		acc.deposit(amount);
+		
+		// Assert
+		Assertions.assertEquals(expectedValue,acc.getBalance());
 	}
 
 }
